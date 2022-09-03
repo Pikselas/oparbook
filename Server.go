d36ebb789e4e136a6fa160493d2ed3d11ec9a7a3
kashikichi <60090083+Pikselas@users.noreply.github.com>
2022-09-03 21:15:31 +0530

    basic logging

--- Server.go
+++ Server.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"server/server"
 )
 
 func ForwardRequestTo(f func(res http.ResponseWriter, req *http.Request)) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
+		log.Printf("\nConnection from\n [%s]\nOn\n [%s]", req.RemoteAddr, req.URL)
 		res.Header().Set("Access-Control-Allow-Origin", "*")
 		f(res, req)
 	}
