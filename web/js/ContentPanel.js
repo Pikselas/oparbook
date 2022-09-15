757d17736adcf47d9200cb8569e7a814d55836a8
kashikichi <60090083+Pikselas@users.noreply.github.com>
2022-09-15 21:13:39 +0530

    Content -Image panel

---
+++ web/js/ContentPanel.js
@@ -0,0 +1,40 @@
+
+let ACTIVE_DRAG_ITEM = null;
+let POS_X = 0;
+let POS_Y = 0;
+
+function ON_DRAGOVER(ev)
+{
+    ev.preventDefault();
+    ACTIVE_DRAG_ITEM.style.top = ev.clientY - POS_Y + "px";
+    ACTIVE_DRAG_ITEM.style.left = ev.clientX - POS_X + "px";
+}
+
+class ContentPanel
+{
+    //Window = null;
+    constructor()
+    {
+        this.Window = document.createElement("div");    
+        this.Window.className = "ContentPanel";
+        this.Window.draggable = true;
+        let tools = document.createElement("div");
+        tools.className = "Tools";
+        this.Window.appendChild(tools);
+        this.Window.ondragstart = this.#dragRegister
+    }
+    #dragRegister(ev)
+    {
+       ACTIVE_DRAG_ITEM = ev.target;
+       POS_X = ev.layerX + 15;
+       POS_Y = ev.layerY;
+    }
+}
+
+class ImagePanel extends ContentPanel
+{
+    constructor()
+    {
+      super();
+    }
+}
---
+++ web/js/editor.js
@@ -0,0 +1,4 @@
+let pn = new ImagePanel();
+document.getElementById("Dp").appendChild(pn.Window);
+
+document.getElementById("Dp").ondragover = ON_DRAGOVER;
